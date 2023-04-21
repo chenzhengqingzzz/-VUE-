6cbaf8f56a6c3455505d87a9d586176303ea5695
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-04-21 18:55:44 +0800

    完成了Home的静态页面搭建，除了TypeNav是全局组件外，其余全是Home的子组件

--- app/src/pages/Home/index.vue
+++ app/src/pages/Home/index.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
-    我是首页
+    <!-- 三级联动全局组件 它已经注册为全局组件了，因此不需要引入 -->
+    <TypeNav/>
+    <!-- 其他子组件 -->
+    <ListContainer/>
+    <TodayRecommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
   </div>
 </template>
 
 <script>
+// 引入其余的组件
+import ListContainer from '@/pages/Home/ListContainer'
+import TodayRecommend from '@/pages/Home/TodayRecommend'
+import Rank from '@/pages/Home/Rank'
+import Like from '@/pages/Home/Like'
+import Floor from '@/pages/Home/Floor'
+import Brand from '@/pages/Home/Brand'
 export default {
-
+  name: 'Home',
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
