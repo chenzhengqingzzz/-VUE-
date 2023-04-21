2cf4038db90926fa05a27e055d336b1c348055bf
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-04-21 17:53:47 +0800

    解决在编程式路由导航多次push、replace后报错的问题，虽然不会影响编码，重写了这两个方法

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -9,6 +9,32 @@ import Home from '../pages/Home'
 import Search from '@/pages/Search'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
+
+// 先把VueRouter原型对象上的push保存一份
+let originPush = VueRouter.prototype.push
+let originReplace = VueRouter.prototype.replace
+// 重写push|replace方法
+// 第一个参数：告诉原来的push方法，往哪里跳转，传递哪些参数
+// 第二个参数：成功的回调
+// 第三个参数：失败的回调
+VueRouter.prototype.push = function(location, resolve, reject) {
+    if (resolve && reject) {
+        // call/apply的区别：
+        // 相同点：都可以调用函数一次，都可以篡改函数的上下文一次
+        // 不同点：call与apply传递参数：call传递参数用逗号隔开，apply方法执行，传递数组
+        originPush.call(this, location, resolve, reject)
+    }else{
+        originPush.call(this, location, () => {}, () => {})
+    }
+}
+VueRouter.prototype.replace = function(location, resolve, reject) {
+    if (resolve && reject) {
+        originReplace.call(this, location, () => {}, () => {})
+    }else{
+        originReplace.call(this, location, () => {}, () => {})
+    }
+}
+
 // 配置路由
 export default new VueRouter({
     routes: [
