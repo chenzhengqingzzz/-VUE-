20093c631ed96cd0d576e6c848c646cdcda5856e
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-05-26 14:33:11 +0800

    交易界面完成

--- app/src/pages/Trade/index.vue
+++ app/src/pages/Trade/index.vue
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-05-25 20:09:59
  * @LastEditors: czqzzzzzz(czq)
- * @LastEditTime: 2023-05-25 21:22:54
+ * @LastEditTime: 2023-05-26 11:34:36
  * @FilePath: /尚硅谷VUE项目实战——尚品汇/app/src/pages/Trade/index.vue
  * @Description: 路由组件————交易结算(Trade)
  * 
@@ -14,28 +14,12 @@
     <h3 class="title">填写并核对订单信息</h3>
     <div class="content">
       <h5 class="receive">收件人信息</h5>
-      <div class="address clearFix">
-        <span class="username selected">张三</span>
-        <p>
-          <span class="s1">北京市昌平区宏福科技园综合楼6层</span>
-          <span class="s2">15010658793</span>
-          <span class="s3">默认地址</span>
-        </p>
-      </div>
-      <div class="address clearFix">
-        <span class="username selected">李四</span>
-        <p>
-          <span class="s1">北京市昌平区宏福科技园综合楼6层</span>
-          <span class="s2">13590909098</span>
-          <span class="s3">默认地址</span>
-        </p>
-      </div>
-      <div class="address clearFix">
-        <span class="username selected">王五</span>
-        <p>
-          <span class="s1">北京市昌平区宏福科技园综合楼6层</span>
-          <span class="s2">18012340987</span>
-          <span class="s3">默认地址</span>
+      <div class="address clearFix" v-for="(address, index) in userAddressList" :key="address.id">
+        <span class="username" :class="{selected: address.isDefault == 1}">{{address.consignee}}</span>
+        <p @click="changeIsDefault(address, userAddressList)">
+          <span class="s1">{{address.fullAddress}}</span>
+          <span class="s2">{{address.phoneNum }}</span>
+          <span class="s3" v-show="address.isDefault == 1">默认地址</span>
         </p>
       </div>
       <div class="line"></div>
@@ -56,40 +40,24 @@
       </div>
       <div class="detail">
         <h5>商品清单</h5>
-        <ul class="list clearFix">
+        <ul class="list clearFix" v-for="(order, index) in orderInfo.detailArrayList" :key="skuId">
           <li>
-            <img src="./images/goods.png" alt="">
+            <img :src="order.imgUrl" alt="" style="width: 100px; height: 100px">
           </li>
           <li>
-            <p>
-              Apple iPhone 6s (A1700) 64G 玫瑰金色 移动联通电信4G手机硅胶透明防摔软壳 本色系列</p>
+            <p>{{order.skuName}}</p>
             <h4>7天无理由退货</h4>
           </li>
           <li>
-            <h3>￥5399.00</h3>
+            <h3>￥{{order.orderPrice}}</h3>
           </li>
-          <li>X1</li>
-          <li>有货</li>
-        </ul>
-        <ul class="list clearFix">
-          <li>
-            <img src="./images/goods.png" alt="">
-          </li>
-          <li>
-            <p>
-              Apple iPhone 6s (A1700) 64G 玫瑰金色 移动联通电信4G手机硅胶透明防摔软壳 本色系列</p>
-            <h4>7天无理由退货</h4>
-          </li>
-          <li>
-            <h3>￥5399.00</h3>
-          </li>
-          <li>X1</li>
+          <li>X{{order.skuNum}}</li>
           <li>有货</li>
         </ul>
       </div>
       <div class="bbs">
         <h5>买家留言：</h5>
-        <textarea placeholder="建议留言前先与商家沟通确认" class="remarks-cont"></textarea>
+        <textarea placeholder="建议留言前先与商家沟通确认" class="remarks-cont" v-model="customerMsg"></textarea>
 
       </div>
       <div class="line"></div>
@@ -102,8 +70,8 @@
     <div class="money clearFix">
       <ul>
         <li>
-          <b><i>1</i>件商品，总商品金额</b>
-          <span>¥5399.00</span>
+          <b><i>{{orderInfo.totalNum}}</i>件商品，总商品金额</b>
+          <span>¥{{orderInfo.originalTotalAmount}}</span>
         </li>
         <li>
           <b>返现：</b>
@@ -116,12 +84,12 @@
       </ul>
     </div>
     <div class="trade">
-      <div class="price">应付金额:　<span>¥5399.00</span></div>
+      <div class="price">应付金额:　<span>¥{{orderInfo.totalAmount}}</span></div>
       <div class="receiveInfo">
         寄送至:
-        <span>北京市昌平区宏福科技园综合楼6层</span>
-        收货人：<span>张三</span>
-        <span>15010658793</span>
+        <span>{{userDeliveryAddress[0].fullAddress}}</span>
+        收货人：<span>{{userDeliveryAddress[0].consignee}}</span>
+        <span>{{userDeliveryAddress[0].phoneNum}}</span>
       </div>
     </div>
     <div class="sub clearFix">
@@ -131,11 +99,49 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
   export default {
     name: 'Trade',
+    data() {
+        return {
+            // 收集买家的留言信息
+            customerMsg: ''
+        }
+    },
     mounted() {
         this.$store.dispatch('trade/getUserAddressList')
         this.$store.dispatch('trade/getOrderInfo')
+    },
+    computed: {
+        ...mapState('trade', ['userAddressList', 'orderInfo']),
+
+        /**
+         * @description: 页面右下角用户配送地址 和上面目前选中的地址信息有关
+         * @return {Array}
+         */
+        userDeliveryAddress(){
+            // 这里可以用filter（返回数组）或find（返回对象）
+            return this.userAddressList.filter((item) => {
+                return item.isDefault == 1
+            })
+        }
+    },
+    methods: {
+        /**
+         * @description: 点击哪一个地址则会设置默认地址
+         * @param {Object} address 点击的目标地址项
+         * @param {Array} userAddressList 总的用户地址数组，其实也可以从组件中获取
+         * @return {*}
+         */
+        changeIsDefault(address, userAddressList){
+            // 排他操作
+            // 全部的isDefault为0
+            userAddressList.forEach(item => {
+                item.isDefault = 0
+                // 点击的目标地址isDefault为1
+                address.isDefault = 1
+            });
+        }
     },
   }
 </script>
